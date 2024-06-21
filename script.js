2cebdec1892ac42e0f8fb0031acad34f501e0458
Sanket Gadade <147973154+sanketgadadeit@users.noreply.github.com>
2024-06-21 17:15:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// function home1(){
+//     var a=document.getElementById('home');
+//     a.setAttribute('style','background:red');
+
+// }
+
+// function about1(){
+//     var a=document.getElementById('home');
+//     a.setAttribute('style','background:red');
+// }
